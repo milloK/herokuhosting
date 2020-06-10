f4faf96dcaa2e1c31775b68c8083d60f84d017ee
millo kuzman <millokuzman@gmail.com>
2020-06-10 13:55:16 +0200

    first commit

---
+++ address_book/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Address(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
